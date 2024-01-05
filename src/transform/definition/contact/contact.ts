35331511e60083d7eb31f781d47bac4e7186c137
Kneesal <nisalcottingham@gmail.com>
2024-01-05 20:44:47 +0000

    fix: map attendee to attender

--- src/transform/definition/contact/contact.ts
+++ src/transform/definition/contact/contact.ts
@@ -2,6 +2,15 @@ import type { FluroDefinitionContact } from '../../../extract/definition/contact
 import type { Cache } from '../../../load/types'
 import type { RockDefinitionContact } from '../../../load/definition/contact'
 
+function transformTitle(title: string): string {
+  switch (title) {
+    case 'Attendee':
+      return 'Attender'
+    default:
+      return title
+  }
+}
+
 export function transform(
   _cache: Cache,
   value: FluroDefinitionContact
@@ -9,7 +18,7 @@ export function transform(
   return {
     IsSystem: false,
     Order: 0,
-    Value: value.title,
+    Value: transformTitle(value.title),
     Description: value.firstLine,
     IsActive: value.status === 'active',
     CreatedDateTime: value.created,
