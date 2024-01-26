b147d71ddc4d4096ff0e227da116c46ec060259e
Kneesal <nisalcottingham@gmail.com>
2024-01-27 10:33:50 +1300

    fix: familyRole dynamic pseudo

---
+++ src/load/contact/familyRole/familyRole.ts
@@ -0,0 +1,15 @@
+// import f from 'odata-filter-builder'
+
+// import { GET, POST, RockApiError } from '../../client'
+
+// let GroupRoleId: number
+
+export async function getFamilyRole(): Promise<void> {
+  //   householdRole: string,
+  //   personId: number,
+  //   fluroId: string
+  // transform housrhold Role into rock role, write transformer function
+  // if null default to 'none'
+  // get group role ID by filtering groups by household Role
+  // if no household role found but householdROle value is not null, create a new value and return the groupRoleID
+}
